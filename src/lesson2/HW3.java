f00b54fc29c4750084916da8a452f216ff870dd0
Andrew <franz.andrewn@gmail.com>
2022-08-27 18:12:16 +0300

    Added lesson2 hw tasks

---
+++ src/lesson2/HW3.java
@@ -0,0 +1,12 @@
+package lesson2;
+
+/*
+Спросить у пользователя одно целое число типа int
+С помощью цикла while вывести столько же раз фразу "Цикл while"
+ */
+public class HW3 {
+
+  public static void main(String[] args) {
+
+  }
+}
