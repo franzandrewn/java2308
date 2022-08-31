a714d1d7e7b352fa74b56a276ed7c4b5b998ac0d
ANikolaev <franz.andrewn@gmail.com>
2022-08-31 19:07:37 +0300

    Added lesson3 hw tasks

---
+++ src/lesson3/HW4.java
@@ -0,0 +1,14 @@
+package lesson3;
+
+/*
+Определить метод arrayN, который принимает массив дробных чисел и целое число n
+и возвращает массив, состоящий из n повторений первого аргумента
+Например если double[] doubleArr = new double[]{1,2,3}
+То arrayN(doubleArr, 3) -> {1,2,3,1,2,3,1,2,3}
+И arrayN(doubleArr, 0) -> {}
+ */
+public class HW4 {
+  public static void main(String[] args) {
+
+  }
+}
