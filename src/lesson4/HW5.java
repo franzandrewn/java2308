defdd804a847d44178c089b9a5147123fbe8f1d3
Andrew Nikolaev <franz.andrewn@gmail.com>
2022-09-06 22:00:26 +0300

    Added lesson4 hw5 answer

--- src/lesson4/HW5.java
+++ src/lesson4/HW5.java
@@ -1,5 +1,7 @@
 package lesson4;
 
+import java.util.Scanner;
+
 /*
 Спросить у пользователя строку с помощью метода nextLine() у сканера
 С помощью методов получившейся строки вывести на экран встречается ли строка "привет" внутри введенной строки
@@ -12,6 +14,20 @@ package lesson4;
 public class HW5 {
 
   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    String input = sc.nextLine();
+    // Если регистр не важен, то
+    String inputLower = input.toLowerCase();
 
+    if (!inputLower.contains("привет")) {
+      System.out.println("Слово 'привет' не встречается");
+    } else if (inputLower.indexOf("привет") == inputLower.lastIndexOf("привет")) {
+      System.out.println("Слово 'привет' встречается единожды");
+
+    } else {
+      System.out.println("Слово 'привет' встречается больше 1 раза");
+      System.out.println("Первый раз встречается на " + inputLower.indexOf("привет"));
+      System.out.println("Последний раз встречается на " + inputLower.lastIndexOf("привет"));
+    }
   }
 }
