7f533937c0fc9e2e6cfc21498aa8fa6b1d1d6f8f
Andrew Nikolaev <franz.andrewn@gmail.com>
2022-09-29 22:01:30 +0300

    Added lesson11 hw tasks Added lesson12 notes

---
+++ src/main/java/lesson11/HW3.java
@@ -0,0 +1,15 @@
+package lesson11;
+
+/*
+Создать запрос к базе данных chinook, который выведет соединенные таблицы artists и albums
+Столбцы для вывода:
+Имя артиста
+Название альбома
+Связь между таблицами - столбец ArtistId в таблице albums
+ */
+public class HW3 {
+
+  public static void main(String[] args) {
+
+  }
+}
